0f320baf0cd618987a563337e9c841e234b04e25
caner <canerakin111@gmaail.com>
2022-02-05 13:26:09 -0500

    initial

---
+++ Notes/Solidity_var_type.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage{
+    uint256 favoriteNumber  =5;
+    bool favoriteBool = true;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0xCc563fb91529F953FCF1e15D9F1aaaF0EA108C50;
+    bytes32 favoritesBytes = "cat";
+}
